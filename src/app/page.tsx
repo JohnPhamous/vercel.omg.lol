74067f2ccf81abf8b55c9dc2eaa76e7f281e1891
John Pham <johnphammail@gmail.com>
2023-01-17 14:39:47 -0800

    Add infinite grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,38 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client";
+import { InfiniteGrid } from "@/components/infinite-grid";
+import { ChakraProvider, Grid, GridItem } from "@chakra-ui/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const IMAGE = "lizzy.gif";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+    <ChakraProvider>
+      <InfiniteGrid>
+        <Grid
+          templateAreas={`
+          "a a a b b b c c"
+          "d d e f f f f f"
+          "g g g h h i i i"
+          "g g g h h i i i"
+        `}
+          gridTemplateColumns={"repeat(8, 1fr)"}
+          gridTemplateRows={"repeat(4, 1fr)"}
+          w={{ base: "150vw", md: "100vw" }}
+          h={{ base: "100vh", md: "125vh" }}
+          gap={8}
+          p={4}
         >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <GridItem area="a" bgImage={IMAGE} bgSize="cover" rounded="3xl" />
+          <GridItem area="b" bgImage={IMAGE} bgSize="cover" rounded="3xl" />
+          <GridItem area="c" bgImage={IMAGE} bgSize="cover" rounded="3xl" />
+          <GridItem area="d" bgImage={IMAGE} bgSize="cover" rounded="3xl" />
+          <GridItem area="e" bgImage={IMAGE} bgSize="cover" rounded="3xl" />
+          <GridItem area="f" bgImage={IMAGE} bgSize="cover" rounded="3xl" />
+          <GridItem area="g" bgImage={IMAGE} bgSize="cover" rounded="3xl" />
+          <GridItem area="h" bgImage={IMAGE} bgSize="cover" rounded="3xl" />
+          <GridItem area="i" bgImage={IMAGE} bgSize="cover" rounded="3xl" />
+        </Grid>
+      </InfiniteGrid>
+    </ChakraProvider>
+  );
 }
