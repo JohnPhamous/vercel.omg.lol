598841cbfe588d266656ce828b3bb5c68c85e241
John Pham <johnphammail@gmail.com>
2023-01-17 15:12:00 -0800

    Add more meta

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,9 +1,11 @@
-const TITLE = "luhsub";
-const DESCRIPTION = "kdevvysyqb luhsub byppo";
+const TITLE = "byppo";
+const DESCRIPTION =
+  "Cuuj Byppo jxu Bypqht, jxu kbjycqju hufjybu qj jxu vehuvhedj ev yddelqjyed. Myjx xuh skjjydw-utwu ijobu qdt vuqhbuii qjjyjktu, Byppo yi qbmqoi fkixydw rekdtqhyui qdt rbqpydw dum jhqybi. Mxujxuh ixu'i sbycrydw jxu xywxuij rhqdsxui eh iskhhoydw jxhekwx jxu bqjuij jusx jhudti, Byppo yi q jhku vehsu je ru husaedut myjx. Vebbem xuh qtludjkhui qdt ijqo qj jxu rbuutydw utwu myjx Byppo jxu Bypqht.";
 const DOMAIN = "https://vercel.omg.lol";
 const OG_STATIC_IMAGE = `${DOMAIN}/og.png`;
 const OG_ANIMATED_IMAGE = `${DOMAIN}/og-animated.gif`;
-const OG_ALT = "A dancing lizard";
+const OG_ALT =
+  "Cuuj Byppo jxu Bypqht, jxu kbjycqju hufjybu qj jxu vehuvhedj ev yddelqjyed. Myjx xuh skjjydw-utwu ijobu qdt vuqhbuii qjjyjktu, Byppo yi qbmqoi fkixydw rekdtqhyui qdt rbqpydw dum jhqybi. Mxujxuh ixu'i sbycrydw jxu xywxuij rhqdsxui eh iskhhoydw jxhekwx jxu bqjuij jusx jhudti, Byppo yi q jhku vehsu je ru husaedut myjx. Vebbem xuh qtludjkhui qdt ijqo qj jxu rbuutydw utwu myjx Byppo jxu Bypqht.";
 const CREATOR = "@johnphamous";
 
 export default function Head() {
@@ -21,20 +23,22 @@ export default function Head() {
       <meta property="og:title" content={TITLE} />
       <meta property="og:description" content={DESCRIPTION} />
       {/* <meta property="og:image" content={OG_STATIC_IMAGE} /> */}
+      <meta name="og:url" content={DOMAIN}></meta>
+      <meta property="og:site_name" content={TITLE}></meta>
 
       <meta property="og:image" content={OG_ANIMATED_IMAGE}></meta>
-
       <meta property="og:image:alt" content={OG_ALT}></meta>
       <meta property="og:image:width" content="600"></meta>
       <meta property="og:image:height" content="600"></meta>
 
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content={DOMAIN}></meta>
       <meta name="twitter:creator" content={CREATOR} />
       <meta name="twitter:title" content={TITLE} />
       <meta name="twitter:description" content={DESCRIPTION} />
       <meta name="twitter:image" content={OG_STATIC_IMAGE} />
       <meta content="Species" name="twitter:label1"></meta>
-      <meta content="🦎 luhsub" name="twitter:data1"></meta>
+      <meta content="🦎 byppo" name="twitter:data1"></meta>
       <meta content="Rarity" name="twitter:label2"></meta>
       <meta content="▲100" name="twitter:data2"></meta>
     </>
